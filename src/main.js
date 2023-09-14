76e212e162ddc0f7f4192fdeb1880a436e067093
BoneWX <2636138358@qq.com>
2023-09-14 10:21:39 +0800

    day2

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import './assets/main.css'
-
+import router from './router'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 引入初始化样式
+import '@/styles/common.scss'
 
 import App from './App.vue'
-import router from './router'
-
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router)
 
 app.mount('#app')
